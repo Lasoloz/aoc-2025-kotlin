ffefe9e093f9be2f210e98a9c322a3974f2654b3
Laszlo Heim <lasolozdev@gmail.com>
2025-12-03 19:01:29 +0200

    [day02] Solve part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -12,40 +12,48 @@ private fun solve(input: String) {
     val result1 = part1(rotations)
     println("Part 1: $result1")
 
-//    val result2 = part2(rotations)
-//    println("Part 2: $result2")
+    val result2 = part2(rotations)
+    println("Part 2: $result2")
 }
 
 private fun part1(input: List<LongRange>): Long {
     return input.asSequence()
         .map { range ->
-//            println("Processing range ${range.start}-${range.endInclusive}")
             range.asSequence()
-                .filter { it.isFromRepeatingDigits() }
-//                .onEach { println("Invalid ID: $it") }
+                .filter { it.isFromRepeatingDigits(times = 2) }
                 .sum()
         }
         .sum()
 }
 
-private fun part2(input: List<LongRange>): Int {
-    return 0
+private fun part2(input: List<LongRange>): Long {
+    return input.asSequence()
+        .map { range ->
+            range.asSequence()
+                .filter { it.isFromRepeatingDigits() }
+                .sum()
+        }
+        .sum()
 }
 
-private fun Long.isFromRepeatingDigits(): Boolean = toString().isFromRepeatingDigits()
+private fun Long.isFromRepeatingDigits(times: Int? = null): Boolean = toString().isFromRepeatingDigits(times)
 
-private fun String.isFromRepeatingDigits(): Boolean {
+private fun String.isFromRepeatingDigits(times: Int? = null): Boolean {
     val verifier = StringBuilder()
     do {
         verifier.append(this[verifier.length])
-        if (this.isRepeatedFrom(verifier, times = 2)) return true
+        if (this.isRepeatedFrom(verifier, times)) return true
     } while (verifier.length < this.length / 2)
 
     return false
 }
 
-private fun String.isRepeatedFrom(part: CharSequence, times: Int): Boolean {
-    if (this.length / part.length != times) {
+private fun String.isRepeatedFrom(part: CharSequence, times: Int? = null): Boolean {
+    if (times != null && this.length / part.length != times) {
+        return false
+    }
+
+    if (this.length / part.length < 2) {
         return false
     }
 
