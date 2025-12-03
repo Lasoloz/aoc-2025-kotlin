cc21a87c3ba97c0bbd6008176ef0f906f27c6e03
Laszlo Heim <lasolozdev@gmail.com>
2025-12-04 00:09:34 +0200

    [day03] Solve part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,15 +8,12 @@ fun main() {
     }
 }
 
-private fun part1(batteryBank: List<BatteryBank>): Int {
-    return batteryBank.sumOf { it.calculateMaxJoltage() }
-}
+private fun part1(batteryBank: List<BatteryBank>): Long = batteryBank.sumOf { it.calculateMaxJoltageOfTwo() }
 
-private fun part2(batteryBank: List<BatteryBank>): Int {
-    return 0
-}
+private fun part2(batteryBank: List<BatteryBank>): Long =
+    batteryBank.sumOf { it.calculateMaxJoltageOf(batteryCount = 12) }
 
-private fun BatteryBank.calculateMaxJoltage(): Int {
+private fun BatteryBank.calculateMaxJoltageOfTwo(): Long {
     var currentMax = 0
     var currentFirst = 0
 
@@ -35,8 +32,39 @@ private fun BatteryBank.calculateMaxJoltage(): Int {
         }
     }
 
-    return currentMax
+    return currentMax.toLong()
 }
+
+private fun BatteryBank.calculateMaxJoltageOf(batteryCount: Int): Long =
+    joltages.findGreatestDigitCombination(batteryCount)
+
+private fun List<Int>.findGreatestDigitCombination(take: Int): Long = findGreatestDigitCombination(0..lastIndex, take)
+    .concatDecimal()
+
+private fun List<Int>.findGreatestDigitCombination(range: IntRange, take: Int): List<Int> {
+    val possibleFirstDigitRange = range.start..(range.endInclusive - (take - 1))
+    val (firstGreatestDigitIndex, digit) = findFirstGreatestDigitIndexedIn(possibleFirstDigitRange)
+    if (take == 1) {
+        return listOf(digit)
+    }
+
+    return listOf(digit) + findGreatestDigitCombination((firstGreatestDigitIndex + 1)..lastIndex, take - 1)
+}
+
+private fun List<Int>.findFirstGreatestDigitIndexedIn(range: IntRange): Pair<Int, Int> {
+    var maxIndex = 0
+    var maxValue = 0
+    for (index in range) {
+        val current = this[index]
+        if (current > maxValue) {
+            maxIndex = index
+            maxValue = current
+        }
+    }
+    return maxIndex to maxValue
+}
+
+private fun List<Int>.concatDecimal() = fold(0L) { acc, curr -> acc * 10L + curr.toLong() }
 
 private fun readInputFile(filename: String): List<BatteryBank> {
     return mutableListOf<BatteryBank>().apply {
