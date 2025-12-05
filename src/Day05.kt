ee56bd5682012ce6d82c6835dca401aa72688b06
Laszlo Heim <lasolozdev@gmail.com>
2025-12-05 10:08:40 +0200

    [day05] Solve part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,12 +8,43 @@ fun main() {
     }
 }
 
-private fun part1(cafeteria: Cafeteria): Int {
-    return cafeteria.ingredients.count { ingredient -> cafeteria.freshRanges.any { range -> ingredient in range }}
+private fun part1(cafeteria: Cafeteria): Long {
+    return cafeteria.ingredients.count { ingredient -> cafeteria.freshRanges.any { range -> ingredient in range } }
+        .toLong()
 }
 
-private fun part2(cafeteria: Cafeteria): Int {
-    return 0
+private fun part2(cafeteria: Cafeteria): Long {
+    val resultingRanges = cafeteria.mergeRanges()
+    // LongRange count is throwing arithmetic overflow exception, that's why we count manually
+    return resultingRanges.sumOf { it.endInclusive - it.start + 1L }
+}
+
+private fun Cafeteria.mergeRanges(): Set<LongRange> {
+    val resultingRanges = mutableSetOf<LongRange>()
+    for (range in freshRanges) {
+        val mergeableRanges = resultingRanges.filter { range mergesWith it }
+        resultingRanges.removeAll(mergeableRanges)
+        val merged = mergeableRanges.fold(range) { acc, current -> acc mergeWith current }
+        resultingRanges.add(merged)
+    }
+    return resultingRanges
+}
+
+private infix fun LongRange.mergesWith(other: LongRange): Boolean =
+    this mergesBefore other || other mergesBefore this || this mergesInside other || other mergesInside this
+
+private infix fun LongRange.mergesBefore(other: LongRange): Boolean =
+    this.start < other.start && this.endInclusive >= other.start
+
+private infix fun LongRange.mergesInside(other: LongRange): Boolean =
+    this.start >= other.start && this.endInclusive <= other.endInclusive
+
+private infix fun LongRange.mergeWith(other: LongRange): LongRange = when {
+    this mergesInside other -> other
+    other mergesInside this -> this
+    this mergesBefore other -> this.start..other.endInclusive
+    other mergesBefore this -> other.start..this.endInclusive
+    else -> throw IllegalArgumentException("Ranges $this and $other cannot be merged!")
 }
 
 private fun readInputFile(filename: String): Cafeteria {
